b6b7376429eccf44c9706ec183e05efa486fb5a1
VidalR <vidalgonz8@gmail.com>
2018-05-14 13:22:52 -0400

    reordered

---
+++ bashing/simpleGC.sh
@@ -0,0 +1,26 @@
+#!usr/bin/env bash
+
+cd ../main/
+mkdir fig/
+mkdir ../../finalsimple
+
+MOD=(b d o a m)
+MNAME=("AVO INTENSO" "AVO DEBIL" "OSTRANDER" "MAZZOTTI A" "MAZZOTTI B")
+Q=(False True)
+
+k=0
+
+for e in ${Q[@]}
+do
+    for j in ${MOD[@]}
+    do
+        echo _______________ MODELO ${MNAME[$k]} Q $e ___________________
+        python3 -u simplemodel_main.py $j $e | tee konsole.log
+        cd ../../
+        tar -czvf finalsimple/simple_$e$j.tar.gz AFVO_RayTrace/
+        cd AFVO_RayTrace/main/
+        rm fig/*.png
+        ((k++))
+    done
+    k=0
+done
---
+++ bashing/wedgeGC.sh
@@ -0,0 +1,26 @@
+#!usr/bin/env bash
+
+cd ../main/
+mkdir fig/
+mkdir ../../finalwedge
+
+MOD=(b d o a m)
+MNAME=("AVO INTENSO" "AVO DEBIL" "OSTRANDER" "MAZZOTTI A" "MAZZOTTI B")
+Q=(False True)
+
+k=0
+
+for e in ${Q[@]}
+do
+    for j in ${MOD[@]}
+    do
+        echo _______________ MODELO ${MNAME[$k]} Q $e ___________________
+        python3 -u wedgemodel_main.py $j $e 170 | tee konsole.log
+        cd ../../
+        tar -czvf finalwedge/wedge$e$j.tar.gz AFVO_RayTrace/
+        cd AFVO_RayTrace/main/
+        rm fig/*.png
+        ((k++))
+    done
+    k=0
+done
